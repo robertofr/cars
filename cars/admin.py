4300797ca62bd2fa5d7f6095a16369197bc88bcc
Roberto F. Rodrigues <robertofr_@hotmail.com>
2026-02-24 14:53:22 -0300

    Aula 38 -Configurações adicionais do nosso projeto

--- cars/admin.py
+++ cars/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from cars.models import Car
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('id', 'model', 'brand', 'factory_year', 'model_year', 'value')
-    search_fields = ('model',)
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ('model','brand')
 
 admin.site.register(Car, CarAdmin)
