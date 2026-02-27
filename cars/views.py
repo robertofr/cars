1641d91c492619d8ea6db7459346b294d7670f41
Roberto F. Rodrigues <robertofr_@hotmail.com>
2026-02-27 17:40:50 -0300

    Aula 51- Consultas com icontains e order_by

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from cars.models import Car, Brand
 
 
-def  cars_view(request):
+def cars_view(request):
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
+
+    if search:
+        cars =  Car.objects.filter(model__icontains=search)
+          
+
     return render(
         request,
          'cars.html', 
-         {'cars':{'model': 'Fusca'}}
+         {'cars': cars}
     )
     
